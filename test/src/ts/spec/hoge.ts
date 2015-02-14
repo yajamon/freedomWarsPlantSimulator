7757db70bf2ff72378ceef2caafc71a437e11604
yajamon <yajamon.tatsuki@gmail.com>
2015-02-14 16:17:15 +0900

    Add empty test

---
+++ test/src/ts/spec/hoge.ts
@@ -0,0 +1,8 @@
+describe('Hoge', function () {
+    describe('hoge', function () {
+        it('empty', function () {
+            console.log('hoge test');
+
+        });
+    });
+});
--- test/src/ts/test_index.ts
+++ test/src/ts/test_index.ts
@@ -1,1 +1,4 @@
+/// <reference path="../../../typings/mocha/mocha.d.ts" />
+/// <reference path="../../../typings/chai/chai.d.ts" />
+
 /// <reference path="spec/hoge.ts" />
