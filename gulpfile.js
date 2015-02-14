bc01aba22c2d4d9d3e450870666cb30e8a51bd45
yajamon <yajamon.tatsuki@gmail.com>
2015-02-14 21:33:43 +0900

    Edit typescript build option

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@
 var gulp = require('gulp');
 var tsc = require('gulp-typescript');
 
+var tscBuildOptions = {
+    target: "ES5",
+    noImplicitAny: true,
+};
+
 gulp.task('copy', function () {
 
     gulp.src('src/html/**')
@@ -10,13 +15,13 @@ gulp.task('copy', function () {
 
 gulp.task('build', ['copy'], function () {
     gulp.src('src/ts/**/*.ts')
-        .pipe(tsc())
+        .pipe(tsc(tscBuildOptions))
         .pipe(gulp.dest('dest/js'));
 });
 
 gulp.task('test-build', ['build'], function () {
     gulp.src('test/src/ts/**/*.ts')
-        .pipe(tsc())
+        .pipe(tsc(tscBuildOptions))
         .pipe(gulp.dest('test/dest/js'));
 });
 
