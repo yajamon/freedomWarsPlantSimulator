4b275ef7f76efc9b2578dc11d06a4edf4ee4b738
yajamon <yajamon.tatsuki@gmail.com>
2015-02-14 16:38:53 +0900

    Add empty task

--- test/src/ts/spec/hoge.ts
+++ test/src/ts/spec/hoge.ts
@@ -1,8 +1,10 @@
+var expect = chai.expect;
+
 describe('Hoge', function () {
     describe('hoge', function () {
         it('empty', function () {
             console.log('hoge test');
-
+            expect([]).to.be.empty;
         });
     });
 });
